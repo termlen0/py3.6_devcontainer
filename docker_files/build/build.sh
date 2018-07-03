032df75dfb6230f2f6a4d04d4e47451baa3a0c9c
Ajay Chenampara <ajay.chenampara@gmail.com>
2018-07-03 09:53:43 -0400

    Updated docker build

--- docker_files/build/build.sh
+++ docker_files/build/build.sh
@@ -16,15 +16,16 @@ pip install --upgrade pip
 # Add python libraries
 pip install paramiko pyyaml jinja2 httplib2 six requests dnspython
 pip install bigsuds f5-sdk netmiko dnspython pysphere
-pip install boto boto3 pytest netaddr
+pip install boto boto3 pytest netaddr ipython
 
 # Install stable ansible
-pip install ansible
+#pip install ansible
+pip install git+https://github.com/ansible/ansible@devel#egg=ansible
 
 
 # Download devel version of Ansible
 
-git clone https://github.com/ansible/ansible.git
+#git clone https://github.com/ansible/ansible.git
 
 # Create the known_hosts and link to /dev/null 
 mkdir ~/.ssh
